c6473d8dee2643b1720f6ac9c3f33159ec2998e6
JonMallozzi <blaze@jons-MacBook-Pro.local>
2019-04-03 02:05:47 -0400

    my algo is bad and does not work, I can fix it with a bunch of shifting math but I think we can do better and simpler. I am going to sleep but here is my code

--- src/student/LoadBalance.java
+++ src/student/LoadBalance.java
@@ -24,7 +24,7 @@ public class LoadBalance
     // In this simple code some tasks will be lost if the number of tasks is not evenly
     // divisible by the number of processors.
     public static int loadBalance(int procs, int[] tasks, int[] partitions)	{	
-        int avg = tasks.length / procs;  // the number of tasks for each processor
+        /*int avg = tasks.length / procs;  // the number of tasks for each processor
 
         // count total work in each partition
         int max = 0;                  // keeps track of the max partition sum
@@ -36,8 +36,100 @@ public class LoadBalance
             partitions[procNum] = total;
             if (total > max) 
                 max = total;
-        }
+                 }*/
+    	
+    	//workload of the largest partition 
+    	int max = 0;
+    	
+    	//total workload of all the tasks added together
+    	int total = 0;
+    	
+    	//stores our partition value
+    	int partTotal = 0;
+    	
+    	//keeps track of what partition to put partTotal into
+    	int partNum = 0;
+    	
+    	for (int cnt = 0; cnt < tasks.length ; cnt++) {
+    		total += tasks[cnt];
+    	}
+    	
+    	//Average workload for each process should be around
+    	int avg = total / procs;
+    	
+    	System.out.println(total);
+    	System.out.println(avg);
+       
+    	for (int cnt = 0; cnt < tasks.length ; cnt++) {
+    	
+    	
+    		System.out.println("partNum: " + tasks[cnt]);	
+    	if (avg > partTotal && ((partTotal + tasks[cnt]) < avg)) {
+    		
+    		partTotal += tasks[cnt];
+    			
+    		
+    	}else if ((avg - partTotal) < ((partTotal + tasks[cnt]) - avg) && partTotal < avg) {
+    		
+    		
+    		//System.out.println("currentTask: " + tasks[cnt]);
+    		//System.out.println("partNum: " + partNum);
+    		
+    		//adding the total to the partition 
+    		partitions[partNum] = partTotal;
+    		
+    		//checks for the max
+        	if (partTotal > max) {
+        		max = partTotal;
+        	}
+    		
+    		//resets the value for the next partition
+    		partNum++;
+    		partTotal = tasks[cnt];
+    		
+    	}else{
+    		
+    		partTotal += tasks[cnt];
+    		
+    		//System.out.println("else partTotal: " + partTotal);
+    		//System.out.println("else cnt: " + cnt);
+    		
+    		//adding the total to the partition 
+    		partitions[partNum] = partTotal;
+    		
+    		//checks for the max
+        	if (partTotal > max) {
+        		max = partTotal;
+        	}
+    		
+    		//resets the value for the next partition
+    		partNum++;
+    		partTotal = 0;
+    		
+    	}
+    	
+    	
+    	
+    }  	
+    	//adds the remaining processes to the last partition if there is extra
+    	if(partNum == procs && partTotal != 0) {
+    		partitions[partNum - 1] += partTotal;
+    		if (partitions[partNum - 1] > max) {
+        		max = partitions[partNum - 1];
+        	}
+    		partTotal = 0;
+    	}
+    	
+    	//adds the last partition value and checks if its max 
+    	if(partNum < procs) {
+    	partitions[partNum] = partTotal;
+    	}
 
+    	if (partTotal > max) {
+    		max = partTotal;
+    	}
+    	
+    	
         return max;
     }
 }
